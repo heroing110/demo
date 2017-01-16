487b05e49fcbc2b414caf660eec4cc2068a757c8
heroing110 <heroing110@163.com>
2017-01-16 16:17:38 +0800

    返回json

---
+++ src/main/java/com/example/bean/User.java
@@ -0,0 +1,25 @@
+package com.example.bean;
+
+/**
+ * Created by lenovo on 2017/1/16.
+ */
+public class User {
+    public String name;
+    public String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,17 +1,20 @@
 package com.example.controller;
 
+import com.example.bean.User;
 import com.example.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by heroing110 on 2017-01-10.
  */
 @Controller
-@RequestMapping(value = "hello")
+@RequestMapping(value = "/demo")
 public class DemoController {
     @Autowired
     private BookService bookService;
@@ -30,4 +33,14 @@ public class DemoController {
         modelMap.put("bookcount",bookcount);
         return "book";
     }
+
+    @ResponseBody
+    @RequestMapping(value = "/getUser")
+    public User getUser(){
+        User user = new User();
+        user.setAge("12");
+        user.setName("小明");
+        return user;
+    }
+
 }
