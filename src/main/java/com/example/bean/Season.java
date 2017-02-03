3a8773c5945673dac0931cecae3c49247d863ad1
heroing110 <heroing110@163.com>
2017-02-03 09:10:44 +0800

    season

---
+++ src/main/java/com/example/bean/Season.java
@@ -0,0 +1,663 @@
+package com.example.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by heying on 2017/2/2 0002.
+ */
+@Entity
+public class Season {
+    @Id
+    @GeneratedValue
+    @Column private Long id;
+    @Column private String cell11;
+    @Column private String cell12;
+    @Column private String cell13;
+    @Column private String cell14;
+    @Column private String cell15;
+    @Column private String cell21;
+    @Column private String cell22;
+    @Column private String cell23;
+    @Column private String cell24;
+    @Column private String cell25;
+    @Column private String cell31;
+    @Column private String cell32;
+    @Column private String cell33;
+    @Column private String cell34;
+    @Column private String cell35;
+    @Column private String cell41;
+    @Column private String cell42;
+    @Column private String cell43;
+    @Column private String cell44;
+    @Column private String cell45;
+    @Column private String cell51;
+    @Column private String cell52;
+    @Column private String cell53;
+    @Column private String cell54;
+    @Column private String cell55;
+    @Column private String cell61;
+    @Column private String cell62;
+    @Column private String cell63;
+    @Column private String cell64;
+    @Column private String cell65;
+    @Column private String cell71;
+    @Column private String cell72;
+    @Column private String cell73;
+    @Column private String cell74;
+    @Column private String cell75;
+    @Column private String cell81;
+    @Column private String cell82;
+    @Column private String cell83;
+    @Column private String cell84;
+    @Column private String cell85;
+    @Column private String cell91;
+    @Column private String cell92;
+    @Column private String cell93;
+    @Column private String cell94;
+    @Column private String cell95;
+    @Column private String cell101;
+    @Column private String cell102;
+    @Column private String cell103;
+    @Column private String cell104;
+    @Column private String cell105;
+    @Column private String cell111;
+    @Column private String cell112;
+    @Column private String cell113;
+    @Column private String cell114;
+    @Column private String o2oRadio;
+    @Column private String cell121;
+    @Column private String mobileRadio;
+    @Column private String cell131;
+    @Column private String cell141;
+    @Column private String cell142;
+    @Column private String cell151;
+    @Column private String cell152;
+    @Column private String cell161;
+    @Column private String cell162;
+    @Column private String cell163;
+    @Column private String cell164;
+    @Column private String cell165;
+    @Column private String companyName;
+    @Column private String year;
+    @Column private String season;
+    @Column private String userid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCell11() {
+        return cell11;
+    }
+
+    public void setCell11(String cell11) {
+        this.cell11 = cell11;
+    }
+
+    public String getCell12() {
+        return cell12;
+    }
+
+    public void setCell12(String cell12) {
+        this.cell12 = cell12;
+    }
+
+    public String getCell13() {
+        return cell13;
+    }
+
+    public void setCell13(String cell13) {
+        this.cell13 = cell13;
+    }
+
+    public String getCell14() {
+        return cell14;
+    }
+
+    public void setCell14(String cell14) {
+        this.cell14 = cell14;
+    }
+
+    public String getCell15() {
+        return cell15;
+    }
+
+    public void setCell15(String cell15) {
+        this.cell15 = cell15;
+    }
+
+    public String getCell21() {
+        return cell21;
+    }
+
+    public void setCell21(String cell21) {
+        this.cell21 = cell21;
+    }
+
+    public String getCell22() {
+        return cell22;
+    }
+
+    public void setCell22(String cell22) {
+        this.cell22 = cell22;
+    }
+
+    public String getCell23() {
+        return cell23;
+    }
+
+    public void setCell23(String cell23) {
+        this.cell23 = cell23;
+    }
+
+    public String getCell24() {
+        return cell24;
+    }
+
+    public void setCell24(String cell24) {
+        this.cell24 = cell24;
+    }
+
+    public String getCell25() {
+        return cell25;
+    }
+
+    public void setCell25(String cell25) {
+        this.cell25 = cell25;
+    }
+
+    public String getCell31() {
+        return cell31;
+    }
+
+    public void setCell31(String cell31) {
+        this.cell31 = cell31;
+    }
+
+    public String getCell32() {
+        return cell32;
+    }
+
+    public void setCell32(String cell32) {
+        this.cell32 = cell32;
+    }
+
+    public String getCell33() {
+        return cell33;
+    }
+
+    public void setCell33(String cell33) {
+        this.cell33 = cell33;
+    }
+
+    public String getCell34() {
+        return cell34;
+    }
+
+    public void setCell34(String cell34) {
+        this.cell34 = cell34;
+    }
+
+    public String getCell35() {
+        return cell35;
+    }
+
+    public void setCell35(String cell35) {
+        this.cell35 = cell35;
+    }
+
+    public String getCell41() {
+        return cell41;
+    }
+
+    public void setCell41(String cell41) {
+        this.cell41 = cell41;
+    }
+
+    public String getCell42() {
+        return cell42;
+    }
+
+    public void setCell42(String cell42) {
+        this.cell42 = cell42;
+    }
+
+    public String getCell43() {
+        return cell43;
+    }
+
+    public void setCell43(String cell43) {
+        this.cell43 = cell43;
+    }
+
+    public String getCell44() {
+        return cell44;
+    }
+
+    public void setCell44(String cell44) {
+        this.cell44 = cell44;
+    }
+
+    public String getCell45() {
+        return cell45;
+    }
+
+    public void setCell45(String cell45) {
+        this.cell45 = cell45;
+    }
+
+    public String getCell51() {
+        return cell51;
+    }
+
+    public void setCell51(String cell51) {
+        this.cell51 = cell51;
+    }
+
+    public String getCell52() {
+        return cell52;
+    }
+
+    public void setCell52(String cell52) {
+        this.cell52 = cell52;
+    }
+
+    public String getCell53() {
+        return cell53;
+    }
+
+    public void setCell53(String cell53) {
+        this.cell53 = cell53;
+    }
+
+    public String getCell54() {
+        return cell54;
+    }
+
+    public void setCell54(String cell54) {
+        this.cell54 = cell54;
+    }
+
+    public String getCell55() {
+        return cell55;
+    }
+
+    public void setCell55(String cell55) {
+        this.cell55 = cell55;
+    }
+
+    public String getCell61() {
+        return cell61;
+    }
+
+    public void setCell61(String cell61) {
+        this.cell61 = cell61;
+    }
+
+    public String getCell62() {
+        return cell62;
+    }
+
+    public void setCell62(String cell62) {
+        this.cell62 = cell62;
+    }
+
+    public String getCell63() {
+        return cell63;
+    }
+
+    public void setCell63(String cell63) {
+        this.cell63 = cell63;
+    }
+
+    public String getCell64() {
+        return cell64;
+    }
+
+    public void setCell64(String cell64) {
+        this.cell64 = cell64;
+    }
+
+    public String getCell65() {
+        return cell65;
+    }
+
+    public void setCell65(String cell65) {
+        this.cell65 = cell65;
+    }
+
+    public String getCell71() {
+        return cell71;
+    }
+
+    public void setCell71(String cell71) {
+        this.cell71 = cell71;
+    }
+
+    public String getCell72() {
+        return cell72;
+    }
+
+    public void setCell72(String cell72) {
+        this.cell72 = cell72;
+    }
+
+    public String getCell73() {
+        return cell73;
+    }
+
+    public void setCell73(String cell73) {
+        this.cell73 = cell73;
+    }
+
+    public String getCell74() {
+        return cell74;
+    }
+
+    public void setCell74(String cell74) {
+        this.cell74 = cell74;
+    }
+
+    public String getCell75() {
+        return cell75;
+    }
+
+    public void setCell75(String cell75) {
+        this.cell75 = cell75;
+    }
+
+    public String getCell81() {
+        return cell81;
+    }
+
+    public void setCell81(String cell81) {
+        this.cell81 = cell81;
+    }
+
+    public String getCell82() {
+        return cell82;
+    }
+
+    public void setCell82(String cell82) {
+        this.cell82 = cell82;
+    }
+
+    public String getCell83() {
+        return cell83;
+    }
+
+    public void setCell83(String cell83) {
+        this.cell83 = cell83;
+    }
+
+    public String getCell84() {
+        return cell84;
+    }
+
+    public void setCell84(String cell84) {
+        this.cell84 = cell84;
+    }
+
+    public String getCell85() {
+        return cell85;
+    }
+
+    public void setCell85(String cell85) {
+        this.cell85 = cell85;
+    }
+
+    public String getCell91() {
+        return cell91;
+    }
+
+    public void setCell91(String cell91) {
+        this.cell91 = cell91;
+    }
+
+    public String getCell92() {
+        return cell92;
+    }
+
+    public void setCell92(String cell92) {
+        this.cell92 = cell92;
+    }
+
+    public String getCell93() {
+        return cell93;
+    }
+
+    public void setCell93(String cell93) {
+        this.cell93 = cell93;
+    }
+
+    public String getCell94() {
+        return cell94;
+    }
+
+    public void setCell94(String cell94) {
+        this.cell94 = cell94;
+    }
+
+    public String getCell95() {
+        return cell95;
+    }
+
+    public void setCell95(String cell95) {
+        this.cell95 = cell95;
+    }
+
+    public String getCell101() {
+        return cell101;
+    }
+
+    public void setCell101(String cell101) {
+        this.cell101 = cell101;
+    }
+
+    public String getCell102() {
+        return cell102;
+    }
+
+    public void setCell102(String cell102) {
+        this.cell102 = cell102;
+    }
+
+    public String getCell103() {
+        return cell103;
+    }
+
+    public void setCell103(String cell103) {
+        this.cell103 = cell103;
+    }
+
+    public String getCell104() {
+        return cell104;
+    }
+
+    public void setCell104(String cell104) {
+        this.cell104 = cell104;
+    }
+
+    public String getCell105() {
+        return cell105;
+    }
+
+    public void setCell105(String cell105) {
+        this.cell105 = cell105;
+    }
+
+    public String getCell111() {
+        return cell111;
+    }
+
+    public void setCell111(String cell111) {
+        this.cell111 = cell111;
+    }
+
+    public String getCell112() {
+        return cell112;
+    }
+
+    public void setCell112(String cell112) {
+        this.cell112 = cell112;
+    }
+
+    public String getCell113() {
+        return cell113;
+    }
+
+    public void setCell113(String cell113) {
+        this.cell113 = cell113;
+    }
+
+    public String getCell114() {
+        return cell114;
+    }
+
+    public void setCell114(String cell114) {
+        this.cell114 = cell114;
+    }
+
+    public String getO2oRadio() {
+        return o2oRadio;
+    }
+
+    public void setO2oRadio(String o2oRadio) {
+        this.o2oRadio = o2oRadio;
+    }
+
+    public String getCell121() {
+        return cell121;
+    }
+
+    public void setCell121(String cell121) {
+        this.cell121 = cell121;
+    }
+
+    public String getMobileRadio() {
+        return mobileRadio;
+    }
+
+    public void setMobileRadio(String mobileRadio) {
+        this.mobileRadio = mobileRadio;
+    }
+
+    public String getCell131() {
+        return cell131;
+    }
+
+    public void setCell131(String cell131) {
+        this.cell131 = cell131;
+    }
+
+    public String getCell141() {
+        return cell141;
+    }
+
+    public void setCell141(String cell141) {
+        this.cell141 = cell141;
+    }
+
+    public String getCell142() {
+        return cell142;
+    }
+
+    public void setCell142(String cell142) {
+        this.cell142 = cell142;
+    }
+
+    public String getCell151() {
+        return cell151;
+    }
+
+    public void setCell151(String cell151) {
+        this.cell151 = cell151;
+    }
+
+    public String getCell152() {
+        return cell152;
+    }
+
+    public void setCell152(String cell152) {
+        this.cell152 = cell152;
+    }
+
+    public String getCell161() {
+        return cell161;
+    }
+
+    public void setCell161(String cell161) {
+        this.cell161 = cell161;
+    }
+
+    public String getCell162() {
+        return cell162;
+    }
+
+    public void setCell162(String cell162) {
+        this.cell162 = cell162;
+    }
+
+    public String getCell163() {
+        return cell163;
+    }
+
+    public void setCell163(String cell163) {
+        this.cell163 = cell163;
+    }
+
+    public String getCell164() {
+        return cell164;
+    }
+
+    public void setCell164(String cell164) {
+        this.cell164 = cell164;
+    }
+
+    public String getCell165() {
+        return cell165;
+    }
+
+    public void setCell165(String cell165) {
+        this.cell165 = cell165;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
